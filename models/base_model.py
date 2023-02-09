9e95d2927a938b42e5f5fc095bdfcc205450a30c
dEstInY-de-Gold <noahayebu@gmail.com>
2023-02-09 19:28:45 +0000

    airbnb test file added

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,17 @@ class BaseModel:
         """
         model initialiser
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at", "name"]:
+                        value = datetime.\
+                                strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def __str__(self):
         """
